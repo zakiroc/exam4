4489f9b562e470faa150f93a2dc495773dc1bf15
zakiroc <164227048+zakiroc@users.noreply.github.com>
2024-06-20 14:31:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,104 @@
+// 1#
+
+// let a = 5
+// let b = 2
+
+// alert(`${a%b}`)
+
+
+
+// 2#
+
+// let a = Math.floor(Math.random() *10)
+// alert(`random orqali chiqgan son : ${a}`)
+
+
+// 3#
+
+// let number = 12.510
+// let hello = Math.floor(number);
+// console.log(hello); 
+
+// 4#
+
+// function countLetters(str) {
+
+//     let le= str.replace(/ /g, '').length;
+//     return le;
+// }
+
+// let iu = "MARS IT SCHOOL";
+// let result = countLetters(iu);
+
+// alert("Количество букв в строке 'MARS IT SCHOOL': " + result);
+
+// 5#
+
+// function pr(phrase, n) {
+//     for (let i = 0; i < n; i++) {
+//         console.log(phrase);
+        
+//     }
+// }
+
+// let pj = "MARS IT SCHOOL";
+// let num = 10;
+
+// pr(pj, num);
+
+// 6#
+
+// let harflar = ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+
+
+// let ism = "zahir";
+
+
+// let ismingiz = ism.toLowerCase().split('').map(char => harflar.includes(char) ? char : '').join('');
+
+// console.log(ismingiz); 
+
+
+// 7#
+
+// let p = prompt('yoshingizni kiriting')
+
+// if(p>18){
+//     alert('Siz balagot yoshiga yetgansiz')
+// }if(p==18){
+//     alert('balogat yoshi muborak')
+// }
+
+
+// 8#
+
+
+// let name = prompt('ismingizni kiriting:');
+
+
+// function rever(str) {
+//   return str.split('').reverse().join('');
+// }
+
+
+// console.log('teskarisi:', rever(name));
+
+
+// 9#
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let evnam = []
+// for (let i = 1; i <= 10; i++) {
+//         if (i % 2 === 0) {
+           
+//             evnam.push(i);
+//         }
+//     }
+//     console.log(sonlar);
+//         console.log(evnam);
+
+
+
+
+
+
